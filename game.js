a97be4265fc76b13ddc9e90006603ac524ec7dc1
dsox1 <richyankee88@gmail.com>
2025-10-31 01:27:43 +0000

    Update game.js

--- game.js
+++ game.js
@@ -665,9 +665,9 @@ function aiTakeTurn() {
       game.current = getNextPlayer(game.current);
       if (game.current !== 'player') setTimeout(aiTakeTurn, 1000);
       return;
-    //}
+    }
     // ✅ If AI can stack, continue with normal play logic
-  }
+  //}
 
 
 
